52af7c5680d87bc6abdad45caba178a8772613d6
Deeppatel-4032 <deeppatel7868@gmail.com>
2025-01-12 20:27:49 +0530

    newDb add

--- db/adminPanel_db.js
+++ db/adminPanel_db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+require("dotenv").config
 
-mongoose.connect("mongodb://localhost:27017/adminPanal_db_data").then(() => {
+mongoose.connect(process.env.MONGODB_URL).then(() => {
     console.log("admin db is connected.....!");
 }).catch((err) => {
     console.log("admin db is not connected.....!", err);
